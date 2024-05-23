1882ed29dc5cb9dc6de6e94722f1559efa8fee67
Matidza M.Z <Matidza46@gmail.com>
2024-05-23 20:01:24 +0200

    Exercises

--- 2-Conditionals/tempCodeRunnerFile.py
+++ 2-Conditionals/tempCodeRunnerFile.py
@@ -1,6 +1,12 @@
-user = "zwivhuya"
-access_level = 3
-if user == "admin" or access_level >= 4:
-    print("Access granted")
+month = input(str("What month is it? "))
+if month in ["September", "October", "November"]:
+    season = 'Autumn'
+elif month in ["December", "January", "February"]:
+    season = 'Winter'
+elif month in ["March", "April", "May"]:
+    season = "Spring"
+elif month in ["June", "July", "August"]:
+    season = "Summer"
 else:
-    print("Acces denied")
+    season = 'Invalid month'
+print(f"The season is {season}")
