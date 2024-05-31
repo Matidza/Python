2bce0c386f216f3cad514c7b5e7592fab942b90f
Matidza M.Z <Matidza46@gmail.com>
2024-05-31 23:01:15 +0200

    Loops- Test

--- 2-Conditionals/Loops/tempCodeRunnerFile.py
+++ 2-Conditionals/Loops/tempCodeRunnerFile.py
@@ -1,5 +1,2 @@
-numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-for num in numbers:
-    if num == 5:
-        print(num)
-        break
+for i in range(1, 11):
+    print(i* '#')
---
+++ 2-Conditionals/Loops/test.py
@@ -0,0 +1,16 @@
+#Exercises: Level 1
+
+# Iterate 0 to 10 using for loop, do the same using while loop.
+
+# Iterate 10 to 0 using for loop, do the same using while loop.
+
+# Write a loop that makes seven calls to print(),
+# so we get on the output the following triangle:
+
+#No 1:
+number = [0,1,2,3,4,5,6,7,8,9,10]
+for i in number:
+    print(i)
+    
+for i in range(11):
+    print(i)
